78030406bc81258e2244381dc867a953f09e771f
René Widera <r.widera@hzdr.de>
2019-02-08 16:20:18 +0100

    disable RBG and scale particle exchanges
    - disable the RNG initialization to not waist time into it
    - scale the memory for the particle exchanges based on the simulation volume per MPI rank

--- include/picongpu/simulation/control/MySimulation.hpp
+++ include/picongpu/simulation/control/MySimulation.hpp
@@ -333,6 +333,11 @@ public:
             bremsstrahlungPhotons<>
         >::type;
 
+/* The random number generator is not used within the SPEC benchmarks.
+ * To keep the initialization enabled would result into a longer initialization than the
+ * benchmark itself on GPUs.
+ */
+#if 0
         // create factory for the random number generator
         const uint32_t userSeed = random::seed::ISeed< random::SeedGenerator >{}();
         const uint32_t seed = std::hash<std::string>{}(
@@ -354,6 +359,7 @@ public:
         pmacc::GridController<simDim>& gridCon = pmacc::Environment<simDim>::get().GridController();
         rngFactory->init( gridCon.getScalarPosition() ^ seed );
         dc.consume( std::move( rngFactory ) );
+#endif
 
         // Initialize synchrotron functions, if there are synchrotron photon species
         if(!bmpl::empty<AllSynchrotronPhotonsSpecies>::value)

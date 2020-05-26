5194fdce6c386bd465dd4a93d23761a6ce1d4214
Jeffrey Kelling <j.kelling@hzdr.de>
2020-05-26 15:24:05 +0200

    ALPAKA_ACC_CPU_BT_OMP4 -> ALPAKA_ACC_ANY_BT_OMP5

--- include/pmacc/traits/GetNumWorkers.hpp
+++ include/pmacc/traits/GetNumWorkers.hpp
@@ -85,7 +85,7 @@ namespace traits
         static constexpr uint32_t value = 1u;
     };
 #endif
-#if( ALPAKA_ACC_CPU_BT_OMP4_ENABLED == 1 ) && defined ALPAKA_OFFLOAD_MAX_BLOCK_SIZE && ALPAKA_OFFLOAD_MAX_BLOCK_SIZE>0
+#if( ALPAKA_ACC_ANY_BT_OMP5_ENABLED == 1 ) && defined ALPAKA_OFFLOAD_MAX_BLOCK_SIZE && ALPAKA_OFFLOAD_MAX_BLOCK_SIZE>0
     template<
         uint32_t T_maxWorkers,
         typename ... T_Args

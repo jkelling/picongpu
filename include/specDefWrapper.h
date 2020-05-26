eb53dc008b80655246616a7c7cf1b42f2dbb1c01
Jeffrey Kelling <j.kelling@hzdr.de>
2020-05-26 15:24:46 +0200

    specDefWrapper.h: Add OpenACC, update OMP5

--- include/specDefWrapper.h
+++ include/specDefWrapper.h
@@ -13,16 +13,17 @@
         // #define ALPAKA_ACC_CPU_B_SEQ_T_OMP2_ENABLED
     #elif defined SPEC_OPENMP_TARGET
         #define CUPLA_STREAM_ASYNC_ENABLED 0
-        #define ALPAKA_ACC_CPU_BT_OMP4_ENABLED
+        #define ALPAKA_ACC_ANY_BT_OMP5_ENABLED
     #elif defined SPEC_CPP11THREADS
         #define CUPLA_STREAM_ASYNC_ENABLED 0
         #define ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLED
     #elif defined SPEC_OPENACC
-        #warning "SPEC_OPENACC not supported, falling back to sequential MPI"
         #define CUPLA_STREAM_ASYNC_ENABLED 0
-        #define ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
+        #define ALPAKA_ACC_ANY_BT_OACC_ENABLED
     #else
         #define CUPLA_STREAM_ASYNC_ENABLED 0
         #define ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
     #endif
+
+    #define ALPAKA_OFFLOAD_MAX_BLOCK_SIZE SPEC_ALPAKA_OFFLOAD_MAX_BLOCK_SIZE
 #endif
